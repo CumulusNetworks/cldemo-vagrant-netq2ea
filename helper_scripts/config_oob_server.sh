0c623ddb202fe4a38bb977fdad7031922eee75d6
Bustin Jetz <44002142+jubetz@users.noreply.github.com>
2019-06-28 14:10:24 -0700

    Update config_oob_server.sh

--- helper_scripts/config_oob_server.sh
+++ helper_scripts/config_oob_server.sh
@@ -335,7 +335,7 @@ sudo apt -y purge cumulus-netq netq-agent netq-apps python-netq-lib
 
 ping 8.8.8.8 -c2
 if [ "\$?" == "0" ]; then
-  echo "deb http://apps3.cumulusnetworks.com/repos/deb CumulusLinux-3 netq-2.1" > /etc/apt/sources.list.d/netq.list
+  echo "deb http://apps3.cumulusnetworks.com/repos/deb CumulusLinux-3 netq-2.2" > /etc/apt/sources.list.d/netq.list
   apt-get update -qy
   apt-get install ntpdate -qy
   apt-get install -yq cumulus-netq
